cf90411cb58b1791d891b8c3bf3a4f2dc4967692
BariBaso <kissmarci41@gmail.com>
2022-01-21 10:43:16 +0100

    1.-2. feladat

---
+++ Fuvar/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class Class1
+    {
+        public int azonosito { get; set; }
+        public DateTime indulás { get; set; }
+        public int ut_ideje { get; set; }
+        public double megtett_táv { get; set; }
+        public double viteldij { get; set; }
+        public double borravalo { get; set; }
+        public string fizetésimoed { get; set; }
+
+        public Class1(string sor)
+        {
+            string[] t = sor.Split(';');
+            azonosito = int.Parse(t[0]);
+            indulás = DateTime.Parse(t[1]);
+            ut_ideje = int.Parse(t[2]);
+            megtett_táv = double.Parse(t[3]);
+            viteldij = double.Parse(t[4]);
+            borravalo = double.Parse(t[5]);
+            fizetésimoed = t[6];
+        }
+    }
+}
---
+++ Fuvar/fuvar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Fuvar
+{
+    class fuvar
+    {
+        static void Main(string[] args)
+        {
+            List<Class1> fuvarok = new List<Class1>();
+            foreach (var x in File.ReadAllLines("fuvar.csv").Skip(1))
+            {
+                fuvarok.Add(new Class1(x));
+            }
+
+
+
+
+        }
+    }
+}
